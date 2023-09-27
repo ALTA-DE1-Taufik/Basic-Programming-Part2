ed2759fb9e7b8209014791a65076bae5a2011f1e
Taufikk <76608559+Taufik-dot@users.noreply.github.com>
2023-09-27 18:40:16 +0700

    Add files via upload

---
+++ PART 2 - Variable, Data Type, Operator/Problem 3.py
@@ -0,0 +1,9 @@
+# Input
+T   = 20.0
+r   = 4.0
+
+
+# Kode Disini
+Pi  = 3.14
+Lp  = 2 * Pi * r**2 + 2*Pi*T*r
+print(Lp)
---
+++ PART 2 - Variable, Data Type, Operator/Problem 4.py
@@ -0,0 +1,7 @@
+Diskon      = 15
+Harga_Awal  = 370000
+
+Harga_Diskon    = (Diskon/100) * Harga_Awal
+Harga_Akhir     = Harga_Awal - Harga_Diskon
+
+print("Harga yang harus dibayar adalah Rp.", Harga_Akhir)
